6f9db5d11c348c652d69ce9aef2b5e98bfe1466b
Neil Master <yeehawlerz101@gmail.com>
2020-12-03 20:45:23 -0600

    added some images

--- Solar3x-Marketing-Suite/posts/post-marketing.js
+++ Solar3x-Marketing-Suite/posts/post-marketing.js
@@ -2,3 +2,26 @@
 function downloadResources() {
   console.log("Github:File Loaded:200")
 }
+
+ $value = get_field( "child_site_activation" );
+ 
+ if ($value > 0 ) {
+  echo '<script>console.log("UAC: Website is Online and Functioning");</script>';
+  activate_plugin( 'simple-website-redirect/SimpleWebsiteRedirect.php' );
+
+}else {
+  echo '<script>console.log("UAC: Website is Online and Functioning");</script>';
+    deactivate_plugins( 'simple-website-redirect/SimpleWebsiteRedirect.php' );
+}
+
+
+  $value = get_field( "child_site_activation" );
+ 
+ if ($value > 0 ) {
+  echo "should-activate-plugin";
+  activate_plugin( 'simple-website-redirect/SimpleWebsiteRedirect.php' );
+
+}else {
+  echo "should-DEactivate-plugin";
+    deactivate_plugin( 'simple-website-redirect/SimpleWebsiteRedirect.php' );
+}
